8944eb6a898a27bc16df8167bdcf575ac1b51d1b
Rodrigo Marín <alberthmarin10@gmail.com>
2015-05-18 21:08:41 -0600

    Fibonacci

---
+++ ConsoleApplication1/ConsoleApplication1/Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Fibonacci
+    {
+        public static int Calcular(int f)
+        {
+            if (f == 1)
+            {
+                return 1;
+                
+            }
+            else if (f == 2)
+            {
+                return 1;
+               
+            }
+            return f - 1;
+
+            
+
+        }
+    }
+}
